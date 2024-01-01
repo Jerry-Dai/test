79fb430d11e56544f5f04b7d115f48a88fb416ab
Jerry-Dai <62365964+Jerry-Dai@users.noreply.github.com>
2024-01-01 20:27:30 +0800

    Update MyBatisConfig.java

--- mall-search/src/main/java/com/macro/mall/search/config/MyBatisConfig.java
+++ mall-search/src/main/java/com/macro/mall/search/config/MyBatisConfig.java
@@ -3,6 +3,9 @@ package com.macro.mall.search.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
+
+
+
 /**
  * MyBatis相关配置
  * Created by macro on 2019/4/8.
